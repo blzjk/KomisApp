5dcf15db471f9c8a79a319eebd7cba58b8679885
Blazej <blzjk@gmail.com>
2022-12-16 23:12:19 +0100

    rozbudowana wyszukiwarka, dodano filtrowanie

--- Aplikacja/Komis/filters.py
+++ Aplikacja/Komis/filters.py
@@ -1,13 +1,29 @@
 import django_filters
+from django_filters import CharFilter
 
 from .models import Samochod
 
 
-class ListingFilter(django_filters.FilterSet):
+class CarFilter(django_filters.FilterSet):
 
+    CHOICES = (
+        ('rosnąco', 'Rosnąco'),
+        ('malejąco', 'Malejąco')
+    )
+    ordering = django_filters.ChoiceFilter(label='Sortuj wg. daty produkcji', choices=CHOICES, method='filter_by_order')
     class Meta:
         model = Samochod
         fields = {
             'marka': ['exact'],
-            'model' : ['exact']
+            'model': ['icontains'],
+            'typ': ['exact'],
+            'paliwo': ['exact'],
+            'kolor': ['exact'],
+            'naped': ['exact'],
+            'cena': ['lt', 'gt'],
+            'opis': ['contains'],
         }
+
+    def filter_by_order(self, queryset, name, value):
+        expression = 'data_produkcji' if value =='rosnąco' else '-data_produkcji'
+        return queryset.order_by(expression)
