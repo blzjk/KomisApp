f141f5e53f95f81625f4c69580f253b39846f859
Blazej <blzjk@gmail.com>
2023-01-18 22:45:09 +0100

    zmiana settings

--- Aplikacja/Aplikacja/settings.py
+++ Aplikacja/Aplikacja/settings.py
@@ -40,7 +40,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bgwrva_rq(!7cn1&w-9w0xgopdjbm=s(5!wb)cio^$s5mlmmqf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -181,8 +181,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'Komis/static'), )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
